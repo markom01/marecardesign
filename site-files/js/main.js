879d079ad220428ec52ba9ea99685a52a9a4f30a
markom01 <markomesa01@gmail.com>
2021-08-27 21:34:40 +0200

    Add loader

---
+++ site-files/js/main.js
@@ -0,0 +1,7 @@
+document.documentElement.onload = function(){
+    document.getElementById("loader").style.display = "block";
+};
+
+window.onload = function(){
+    document.getElementById("loader").style.display = "none";
+};
