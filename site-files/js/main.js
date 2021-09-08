cd6bc36ef35556e4ecffe8ecf046c298ed2cb078
markom01 <markomesa01@gmail.com>
2021-09-08 13:41:29 +0200

    Add footer and temporary remove loader

--- site-files/js/main.js
+++
@@ -1,7 +0,0 @@
-document.documentElement.onload = function(){
-    document.getElementById("loader").style.display = "block";
-};
-
-window.onload = function(){
-    document.getElementById("loader").style.display = "none";
-};
